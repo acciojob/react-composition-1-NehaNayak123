5ad26e7fc26ac07612643caf7c0dfea26823d96c
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-04-14 01:55:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
 
-import React from "react";
+import React, {useState} from "react"
 import './../styles/App.css';
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
+// const App = () => {
+//   return (
+//     <div>
+//         {/* Do not remove the main div */}
+//     </div>
+//   )
+// }
+
+// export default App
+
+const data=["Tab 1", "Tab 2", "Tab 3"]
+export  function App() {
+    const [state, setState]=useState(data[0])
+    const stateChange = (item)=>{
+        setState(()=>item)
+    }
+  return <div>
+              <ul>
+                  {
+                      data.map((item)=>(
+                          <li onClick={()=>stateChange(item)} key={item}>{item}</li>
+                      ))
+                  }
+              </ul>
+          <p>This is the content for {state}</p>
     </div>
-  )
 }
+
 
 export default App
